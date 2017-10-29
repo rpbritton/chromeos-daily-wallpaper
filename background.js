63100efb376bd8e8719755ac500707e1031a8a58
rpbritton <rpbritton@outlook.com>
2017-10-28 23:25:48 -0400

    working, successful version

--- background.js
+++ background.js
@@ -14,20 +14,28 @@ function xhrGet(url) {
 	});
 }
 
-xhrGet('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US').then(response => {
-	chrome.wallpaper.setWallpaper({
-		'url': `http://www.bing.com${JSON.parse(response).images[0].url}`,
-		'layout': 'CENTER_CROPPED',
-		'filename': 'bing wallpaper'
-	});
-}).catch(error => {
-	
+chrome.alarms.create({
+	'when': Math.ceil(Date.now()/1000/60/60/24)*24*60*60*1000,
+	'periodInMinutes': 1440
+});
+chrome.alarms.onAlarm.addListener(alarm => {
+	getWallpaper();
 });
 
-chrome.wallpaper.setWallpaper({
-	'url': `background.jpg`,
-	'layout': 'CENTER_CROPPED',
-	'filename': 'bing_wallpaper'
-}, () => {
-	console.log("wallpaper set!");
-});
+function getWallpaper() {
+	xhrGet('http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US').then(response => {
+		chrome.wallpaper.setWallpaper({
+			'url': `http://www.bing.com${JSON.parse(response).images[0].url}`,
+			'layout': 'CENTER_CROPPED',
+			'filename': 'bing_wallpaper'
+		}, () => {
+			console.log("wallpaper set!");
+		});
+	}).catch(error => {
+		console.error("could not fetch wallpaper, retrying in 30 seconds");
+		setTimeout(() => {
+			getWallpaper();
+		}, 30000);
+	});
+}
+getWallpaper();
